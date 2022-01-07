1ca6ef1bfebb7b0257ca11c4eec7a032746b9621
yadnyesh <yadnyesh.juvekar@gmail.com>
2022-01-07 19:16:25 +0530

    Implemented saving product in Product Events Handler

--- src/main/java/io/yadnyesh/productservice/command/api/events/ProductEventsHandler.java
+++ src/main/java/io/yadnyesh/productservice/command/api/events/ProductEventsHandler.java
@@ -1,13 +1,25 @@
 package io.yadnyesh.productservice.command.api.events;
 
+import io.yadnyesh.productservice.command.api.data.Product;
+import io.yadnyesh.productservice.command.api.data.ProductRepository;
 import org.axonframework.eventhandling.EventHandler;
+import org.springframework.beans.BeanUtils;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ProductEventsHandler {
 
+    private ProductRepository productRepository;
+
+    public ProductEventsHandler(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
     @EventHandler
     public void on(ProductCreatedEvent productCreatedEvent) {
-
+        Product product = new Product();
+        BeanUtils.copyProperties(productCreatedEvent, product);
+        productRepository.save(product);
     }
 }
