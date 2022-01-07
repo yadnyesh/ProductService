bf18794e02e8c72c715a562e8b4bdb44c372e3e2
yadnyesh <yadnyesh.juvekar@gmail.com>
2022-01-07 19:33:53 +0530

    Fixed missing Annotation

--- src/main/java/io/yadnyesh/productservice/command/api/aggregate/ProductAggregate.java
+++ src/main/java/io/yadnyesh/productservice/command/api/aggregate/ProductAggregate.java
@@ -2,6 +2,7 @@ package io.yadnyesh.productservice.command.api.aggregate;
 
 import io.yadnyesh.productservice.command.api.commands.CreateProductCommand;
 import io.yadnyesh.productservice.command.api.events.ProductCreatedEvent;
+import org.axonframework.commandhandling.CommandHandler;
 import org.axonframework.eventsourcing.EventSourcingHandler;
 import org.axonframework.modelling.command.AggregateIdentifier;
 import org.axonframework.modelling.command.AggregateLifecycle;
@@ -21,6 +22,7 @@ public class ProductAggregate {
     public ProductAggregate() {
     }
 
+    @CommandHandler
     public ProductAggregate(CreateProductCommand createProductCommand) {
         ProductCreatedEvent productCreatedEvent = new ProductCreatedEvent();
         BeanUtils.copyProperties(createProductCommand, productCreatedEvent);
