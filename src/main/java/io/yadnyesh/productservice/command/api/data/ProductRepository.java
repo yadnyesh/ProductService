22de1fdf470ac598ff93692e203377ede1233613
yadnyesh <yadnyesh.juvekar@gmail.com>
2022-01-07 19:14:40 +0530

    Created Product Repository

---
+++ src/main/java/io/yadnyesh/productservice/command/api/data/ProductRepository.java
@@ -0,0 +1,6 @@
+package io.yadnyesh.productservice.command.api.data;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, String> {
+}
