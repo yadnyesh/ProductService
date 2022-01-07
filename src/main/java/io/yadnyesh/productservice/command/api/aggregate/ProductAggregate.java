91d0312c6de972e21fbbc0d679514b6bc61415ec
yadnyesh <yadnyesh.juvekar@gmail.com>
2022-01-07 19:06:55 +0530

    Added code to publish to EventStore

--- src/main/java/io/yadnyesh/productservice/command/api/aggregate/ProductAggregate.java
+++ src/main/java/io/yadnyesh/productservice/command/api/aggregate/ProductAggregate.java
@@ -1,7 +1,12 @@
 package io.yadnyesh.productservice.command.api.aggregate;
 
+import io.yadnyesh.productservice.command.api.commands.CreateProductCommand;
+import io.yadnyesh.productservice.command.api.events.ProductCreatedEvent;
+import org.axonframework.eventsourcing.EventSourcingHandler;
 import org.axonframework.modelling.command.AggregateIdentifier;
+import org.axonframework.modelling.command.AggregateLifecycle;
 import org.axonframework.spring.stereotype.Aggregate;
+import org.springframework.beans.BeanUtils;
 
 import java.math.BigDecimal;
 
@@ -12,4 +17,21 @@ public class ProductAggregate {
     private String name;
     private BigDecimal price;
     private Integer quantity;
+
+    public ProductAggregate() {
+    }
+
+    public ProductAggregate(CreateProductCommand createProductCommand) {
+        ProductCreatedEvent productCreatedEvent = new ProductCreatedEvent();
+        BeanUtils.copyProperties(createProductCommand, productCreatedEvent);
+        AggregateLifecycle.apply(productCreatedEvent);
+    }
+
+    @EventSourcingHandler
+    public void on(ProductCreatedEvent productCreatedEvent) {
+        this.quantity = productCreatedEvent.getQuantity();
+        this.productId = productCreatedEvent.getProductId();
+        this.price = productCreatedEvent.getPrice();
+        this.name = productCreatedEvent.getName();
+    }
 }
